3b6ce1c41aa8b24886c6e3c8cc4ba4474b8e09a8
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-06 15:18:09 +0100

    Started working on call to action Component

--- src/Components/CallToAction/CallToAction.jsx
+++ src/Components/CallToAction/CallToAction.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./CallToAction.css";
+import iconLeft from "../../assets/images/icon-left.png";
+import iconRight from "../../assets/images/icon-right.png";
 
 const CallToAction = () => {
   return (
     <div className="cta-container">
       <div className="cta-text">
+        <img src={iconLeft} alt="" className="icon-left" />
         <h2 className="first-text">WHAT IS</h2>
-        <h2>MATCH</h2>
+        <h2 className="second-text">MATCH</h2>
+        <img src={iconRight} alt="" className="icon-right" />
       </div>
     </div>
   );
