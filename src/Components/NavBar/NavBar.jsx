d678a42cb09f60503d3489f4cb410d3b4252e7ab
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-06 14:18:16 +0100

    Started adding assets

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import logo from "../../assets/images/LOGO.png"
+import logo from "../../assets/images/LOGO.png";
 
-const NavBar = () =>{
-    return(
-       <header>
-        <div className="navbar-container">
-            <a href="##" className="logo"><img src={logo} alt="" /></a>
-        </div>
-       </header>
-    )
-}
+const NavBar = () => {
+  return (
+    <header>
+      <div className="navbar-container">
+        <a href="https://matchain.io" className="logo">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+    </header>
+  );
+};
 
-export default NavBar
+export default NavBar;
