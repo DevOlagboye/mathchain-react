3d3970ad99b4b199475734a1b5c81443d6866952
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-06 14:17:52 +0100

    Started adding assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../assets/images/LOGO.png"
+
+const NavBar = () =>{
+    return(
+       <header>
+        <div className="navbar-container">
+            <a href="##" className="logo"><img src={logo} alt="" /></a>
+        </div>
+       </header>
+    )
+}
+
+export default NavBar
