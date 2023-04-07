a794e62cd180dd225f7eb7ed8e54b987957d8a17
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-07 15:36:29 +0100

    Finished the Match Component

--- src/Components/Match/Match.jsx
+++ src/Components/Match/Match.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Match.css";
 import moonImage from "../../assets/images/moon.png";
+import matchImage from "../../assets/images/match-gif.gif";
 
 const Match = () => {
   return (
@@ -13,6 +14,24 @@ const Match = () => {
           Connections
         </h3>
         <img src={moonImage} alt="Match Moon" className="moon-image" />
+      </div>
+      <div className="match-text-image">
+        <div className="match-text-details">
+          <h5>
+            Match's modularity enables the seamless <br />
+            integration of new features and upgrades <br />
+            without compromising user-owned content
+            <br />
+            and social relationships. Ultimately, Match
+            <br /> offers a promising solution for creators seeking <br />
+            to build and maintain authentic relationships
+            <br /> with their community on a decentralized <br />
+            platform.
+          </h5>
+        </div>
+        <div className="match-image">
+          <img src={matchImage} alt="" />
+        </div>
       </div>
     </div>
   );
