c9c8f4f1ce54086bae58bb29ebba4140f2b4a3a1
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-07 14:47:24 +0100

    Started working on the Match Component

--- src/Components/Match/Match.jsx
+++ src/Components/Match/Match.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import "./Match.css"
+import React from "react";
+import "./Match.css";
+import moonImage from "../../assets/images/moon.png";
 
 const Match = () => {
   return (
-    <div>Match</div>
-  )
-}
+    <div className="match-container">
+      <div className="match-text">
+        <h3>
+          Match's Modularity <br />
+          ensures Seamless <br />
+          Upgrades and Authentic <br />
+          Connections
+        </h3>
+        <img src={moonImage} alt="Match Moon" className="moon-image" />
+      </div>
+    </div>
+  );
+};
 
-export default Match
+export default Match;
