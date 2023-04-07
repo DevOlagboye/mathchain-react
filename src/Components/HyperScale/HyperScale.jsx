dc1fda60294c4b56a0fd65e34fe93d7626ca59b1
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-07 10:29:47 +0100

    Started working on the HyperScale Component

--- src/Components/HyperScale/HyperScale.jsx
+++ src/Components/HyperScale/HyperScale.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import "./HyperScale.css"
+import React from "react";
+import "./HyperScale.css";
+import hyperImage from "../../assets/images/hyper-text-image.png";
 
 const HyperScale = () => {
   return (
-    <div>HyperScale</div>
-  )
-}
+    <div className="hyperscale-container">
+      <div className="hyperscale-main-text">
+        <h3>
+          Hyper-scalable social
+          <br /> graph for seamless <br />
+          connection, collaboration,
+          <br /> and innovation.
+        </h3>
+        <img src={hyperImage} alt="Hyper Icon" className="hyper-img" />
+      </div>
+    </div>
+  );
+};
 
-export default HyperScale
+export default HyperScale;
