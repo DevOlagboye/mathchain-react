968115e5946253fd67dd4c7c93af04402efe8a2d
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-07 13:59:30 +0100

    Started styling  the Non Zero  Component

--- src/Components/NonZero/NonZero.jsx
+++ src/Components/NonZero/NonZero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./NonZero.css";
+import eclipseImg from "../../assets/images/eclipse.png";
 
 const NonZero = () => {
   return (
     <div className="non-zero-container">
       <div className="non-zero-text">
+        <img src={eclipseImg} alt="" className="eclipse-image" />
         <h3>
           Non-zero-sum Game : <br /> Collaboration Over <br />
           Competition
