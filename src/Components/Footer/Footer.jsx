2e3b19a38ea2771f8ab993550d8a8a201eb0862a
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-08 11:23:39 +0100

    Started working on the Footer Component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <div>Footer</div>
+    </div>
+  );
 };
 
 export default Footer;
