2565fb14741b61802496f2f7dd0f76cfbf58d981
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-15 14:15:15 +0100

    Working on responsiveness of the Newworld Component

--- src/Components/NewWorld/NewWorld.jsx
+++ src/Components/NewWorld/NewWorld.jsx
@@ -11,7 +11,7 @@ const NewWorld = () => {
         </h5>
         <h5 className="second-new-world-text">OF CONNECTED TRAFFIC</h5>
         <p>Building the Decentralized Social Network We Truly Deserve</p>
-        <img src={newWorldImage} alt="" />
+        <img src={newWorldImage} alt="" className="new-world-svg" />
       </div>
     </div>
   );
